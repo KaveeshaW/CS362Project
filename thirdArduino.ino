22ba5b616081a4761fe06665a2987a786bf77e1b
azaval9 <47799142+azaval9@users.noreply.github.com>
2019-04-12 21:17:14 -0500

    Arduino #3 initial upload
    Third Arduino's code still needs to be written and "security" is to be confirmed

---
+++ thirdArduino.ino
@@ -0,0 +1,18 @@
+/* TODO: ARDUINO #3
+ *  planned: have a lcd or timer, buzzer, button, and mulitple Leds that 
+ *  give a ten second countdown before a buzzer and leds are activated. 
+ *  LCD will display a a message that law enforcement was called.
+ */
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
